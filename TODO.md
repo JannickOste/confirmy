3a63967685bf03a1373746e0647b8716b75c2663
Jannick Oste <jannick@oste.dev>
2024-02-03 23:02:29 +0100

    Todo added, needs expanding

---
+++ TODO.md
@@ -0,0 +1,8 @@
+
+- Add deserialization for JSON/YAML. 
+- Add string validation tests. 
+- Add decorator support. 
+- Add date support 
+- Add DateTime support 
+- Optimize performance
+- Add logging support
--- src/tests/dummy/TestObject.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TestObject {
-    constructor() {
-        this.someProperty = "";
-    }
-}
-exports.default = TestObject;
-//# sourceMappingURL=TestObject.js.map
